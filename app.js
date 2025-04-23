adcefcded97a5aacc4bafe6b75b8f7a1f9561b7f
Bruna Borges <bruna.lborges.matias@gmail.com>
2025-04-22 21:53:53 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+//import mongoose from 'mongoose'
+
+const app = express()
+const admin = require('./routes/admin')
+const path = require('path')
+
+//Configs  
+    app.use(express.urlencoded({extended: true}))
+    app.use(express.json())
+
+    //Handlebars
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars');
+    app.set('views', './views');
+
+    //public
+    app.use(express.static(path.join(__dirname + "/public")))
+
+
+//rotas
+app.get('/',(req,res) => {
+    res.send("Rota principal")
+})
+app.use('/admin', admin)
+
+
+
+app.listen(3000, () => {
+    console.log("Servidor rodando")
+})
