10382f21b02d9e48a91f7887bd0c880d6155ad8b
Bruna Borges <bruna.lborges.matias@gmail.com>
2025-05-07 14:19:21 -0300

    adicionando pagina inicial e pagina de detalhes de uma postagem

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const flash = require('connect-flash')
 const app = express()
 const admin = require('./routes/admin')
 const path = require('path')
+require("./models/Postagem")
+const Postagem = mongoose.model('postagens')
 
 //Configs  
     //sessao
@@ -46,8 +48,34 @@ const path = require('path')
 
 //rotas
 app.get('/',(req,res) => {
-    res.send("Rota principal")
+    Postagem.find().lean().populate("categoria").sort({data: "desc"}).then((postagens) => {
+        res.render("index", {postagens:postagens})
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno")
+        res.redirect("/404")
+    })
+    
 })
+
+app.get("/postagem/:slug", (req,res) => {
+    Postagem.findOne({slug: req.params.slug}).lean().then((postagem) => {
+        if(postagem){
+            res.render("postagem/index", {postagem: postagem})
+        }
+        else {
+            res.flash("error_msg", "Essa postagem não existe")
+            res.redirect("/")
+        }
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno")
+        res.redirect("/")
+    })
+})
+
+app.get("/404", (req,res) => {
+    res.send("Erro 404")
+})
+
 app.use('/admin', admin)
 
 app.listen(3000, () => {
