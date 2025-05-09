7498596004d594941ddf8177122fb7f18469d95a
Bruna Borges <bruna.lborges.matias@gmail.com>
2025-05-09 18:41:17 -0300

    construindo rota de cadastro

--- models/Usuario.js
+++ models/Usuario.js
@@ -13,6 +13,10 @@ const Usuario = new Schema({
     senha: {
         type: String,
         required: true
+    },
+    adm: {
+        type: Number,
+        default: 0
     }
 })
 
--- routes/usuario.js
+++ routes/usuario.js
@@ -9,4 +9,34 @@ router.get("/cadastro", (req,res) => {
     res.render("usuarios/cadastro")
 })
 
+router.post("/cadastro", (req,res) => {
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome inválido"})
+    }
+
+    else if(!req.body.email || typeof req.body.email == undefined || req.body.email == null){
+        erros.push({texto: "Email inválido"})
+    }
+
+    else if(!req.body.senha || typeof req.body.senha == undefined || req.body.senha == null){
+        erros.push({texto: "Senha inválida"})
+    }
+
+    else if(req.body.senha.length < 4){
+        erros.push({texto: "Senha muito curta"})
+    }
+
+    else if(req.body.senha != req.body.senha2){
+        erros.push({texto: "As senhas não correspondem"})
+    }
+
+    if(erros.length > 0) {
+        res.render("usuarios/cadastro", {erros:erros})
+    }else {
+
+    }
+})
+
 module.exports = router
